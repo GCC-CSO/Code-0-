4d838133025ec426bbc36bb014723ce56edfe054
Luke Wolf <lukewolf101010@gmail.com>
2013-11-26 15:53:16 -0700

    Add an armor class to the project

--- Tools/ItemGenerator/Armor.cs
+++ Tools/ItemGenerator/Armor.cs
@@ -18,36 +18,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Runtime.Serialization;
 
-
-namespace Code0
-{    
-    [DataContract]
-    class CharacterAttributes
+namespace ItemGenerator
+{
+    using Code0;
+    class Armor
     {
-        #region constructors        
-        public CharacterAttributes()
+        #region constructors
+        public Armor()
         {
-            Fortitude = new Decimal(0);
-            Strength = new Decimal(0);
-            Agility = new Decimal(0);
+            Resistance = new CombatAttributes();
+            Soak = new CombatAttributes();
+            StatBoost = new CharacterAttributes();
+            SkillBoost = new WeaponSkills();
         }
-        public CharacterAttributes(decimal Fortitude, decimal Strength, decimal Agility)
-        {            
-            this.Fortitude = Fortitude;
-            this.Strength = Strength;
-            this.Agility = Agility;
+
+        public Armor(CombatAttributes Resistance, CombatAttributes Soak, CharacterAttributes StatBoost, WeaponSkills SkillBoost)
+        {
+            this.Resistance = Resistance;
+            this.Soak = Soak;
+            this.StatBoost = StatBoost;
+            this.SkillBoost = SkillBoost;
         }
         #endregion
-
         #region properties
-        [DataMember]
-        public decimal Fortitude{get; set;}
-        [DataMember]
-        public decimal Strength{get; set;}
-        [DataMember]
-        public decimal Agility{ get; set; }
+        public CombatAttributes Resistance { get; set; }
+        public CombatAttributes Soak { get; set; }
+        public CharacterAttributes StatBoost { get; set; }
+        public WeaponSkills SkillBoost { get; set; }
         #endregion
     }
 }
